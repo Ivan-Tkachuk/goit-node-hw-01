c0380740a53ecd4aa7ecaf22f7796f014d3d9649
Ivan-Tkachuk <tkachuchella@gmail.com>
2023-05-04 14:24:46 +0300

    goit-node-hw-01

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# goit-node-hw-01
+Screenshots of running a programm.
+
+list - https://ibb.co/7NJQSnz
+get - https://ibb.co/pWDX8Dc
+add - https://ibb.co/Cz5Jz53
+remove - https://ibb.co/8XKFWP1
+
+
+
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const contacts = require("./contacts/contacts");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 
-const invokeAction = async ({action, id, name, email, phone}) => {
-    switch(action) {
-        case "read":
-            const allContacts = await contacts.listContacts();
-            return console.log(allContacts);
-    }
-}
+const contacts = require("./contacts");
 
-invokeAction({action: "read"});
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.table(newContact);
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.table(deleteContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
